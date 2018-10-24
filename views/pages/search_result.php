2f110de8a894859ca707fa5c28ad7a5c461333e8
Ivan Fadillah <ifadillah90@gmail.com>
2018-10-24 10:11:42 +0700

    add fitur search_result

---
+++ views/pages/search_result.php
@@ -0,0 +1,13 @@
+<?php
+  require_once('../../Config/Config.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8" />
+		<title>Search Result</title>
+		<link rel="stylesheet" href="../../public/css/search_result.css"/>
+    </head>
+    <body>
+        <h1>Search Result</h1>
