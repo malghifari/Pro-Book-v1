1a7d52006324288554da38dd884f894c72c5ff98
Ivan Fadillah <ifadillah90@gmail.com>
2018-10-24 10:27:13 +0700

    add header php

---
+++ views/pages/header.php
@@ -0,0 +1,26 @@
+<?php
+  require_once('../../Config/Config.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8" />
+		<title>Search</title>
+        <link rel="stylesheet" href="../../public/css/header.css"/>
+        
+    </head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"/>
+    <body>
+    <div class ="tabcontent">
+        <h1><b style = "font-size : 35pt; color : #FFFF00";>Pro-</b><b style= "font-size : 35pt";>Book</b><button class="logout" type="submit" ><i class = "fas fa-power-off" style = "font-size : 25px";></i></button></h1>
+        <div class="navbar">
+            <a class="active" href="#">Browse</a> 
+            <a href="#">History</a> 
+            <a href="#">Profile</a> 
+         </div>
+    </div>
+    
+    
+</html>
+    
