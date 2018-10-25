55f1dc7c3e3043591a7b1237cbae8065c4bfbeab
daandeey <dandyarifrahman@gmail.com>
2018-10-25 14:45:07 +0700

    fix history.php

--- views/pages/History.php
+++ views/pages/History.php
@@ -2,6 +2,7 @@
 	session_start();
 	$_SESSION['page'] = 'History';
 	include '../../Config/Config.php';
+	include Config::DOCUMENT_ROOT . '/Controller/HistoryController.php'
 ?>
 
 <!DOCTYPE html>
@@ -16,24 +17,42 @@
 		<div class="frame">
             <?php include Config::DOCUMENT_ROOT . "/views/includes/Header.php"?>
 			<h1 class="history-header">History</h1>
-			<div>
-				<div class="row">
-					<div class="avatar-collumn">
-						<div class="avatar">
-							
-						</div>
-					</div>
-					<div class="description">
-						<h2>Nota Hidup </h2>
-						<p>Jumlah: 1</p><br>
-						<p>Anda sudah memberikan review</p>
-					</div>
-					<div class="details">
-						<p>30 September 2018</p><br>
-						<p>Nomor Order : #2</p>
-					</div>
-				</div>
-			</div>
+			<?php
+                $orders = HistoryController::fetchOrder($_COOKIE['username']);
+                foreach ($orders as $order) {
+                    if($order['flag'] == 0) {
+                        $Reviewed = 'Belum direview';
+                        $Reviewbutton = '';
+                    } else {
+                        $Reviewed = 'Anda sudah memberikan review';
+                        $Reviewbutton = '<a href="" class="review-button"></a>';
+                    }
+
+                    echo "    <div>
+                                <div class='row'>
+                                    <div class='avatar-collumn'>
+                                        <div class='avatar'>
+                                            <img src='$order['avatar']\'>
+                                        </div>
+                                    </div>
+                                    <div class='description'>
+                                        <h2>$order['book-name']</h2>
+                                        <p>Jumlah: $order['quantity']</p><br>
+                                        <p>$Reviewed</p>
+                                    </div>
+                                    <div class='details'>
+                                        <p>$order['date']</p><br>
+                                        <p>Nomor Order : #$order['order-no']</p>
+                                        $Review-button
+                                    </div>
+                                </div>
+                            </div>"
+                }
+            ?>
+
+
+
+
 		</div>
 	</body>
 </html>
