6fc56dfea97a5de2408b8fb868def5bca31887be
alghi <13516038@std.stei.itb.ac.id>
2018-10-25 17:35:01 +0700

    Added phone number validation

--- public/js/Register.js
+++ public/js/Register.js
@@ -22,12 +22,14 @@ function elementOnChange(element) {
   var query = element.id + '=' + element.value;
   var url = 'http://localhost/WBD1/tugasbesar1_2018' + '/Controller/RegisterController.php?' + query;
   xmlhttp.open('GET', url, true);
-  xmlhttp.onreadystatechange = usernameRSC;
+  xmlhttp.onreadystatechange = function() {
+    elementRSC(element.id);
+  }
   xmlhttp.send(null);    
 }
 
-function usernameRSC() {
-  document.querySelector('.status-username').innerHTML = xmlhttp.responseText;
+function elementRSC(elementid) {
+  document.querySelector('.status-' + elementid).innerHTML = xmlhttp.responseText;
 }
 
 var username = document.querySelector('#username');
@@ -37,14 +39,31 @@ username.onchange = function() {
     return;
   }
   elementOnChange(username);
+  return;
 }
 
 var email = document.querySelector('#email');
 email.onchange = function() {
   var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
-  if (re.test(String(email.value))) {
+  if (!re.test(String(email.value))) {
     document.querySelector('.status-email').innerHTML = "Email not valid";
     return;
   }
   elementOnChange(email);
+  return;
+}
+
+var phone_number = document.querySelector('#phone-number');
+phone_number.onchange = function() {
+  var re = /[0-9]/;
+  if (!re.test(String(phone_number.value))) {
+    document.querySelector('.status-phone-number').innerHTML = "Input type must be number";
+    return;
+  }
+  if (phone_number.value.length < 9 || phone_number.value.length > 12)  {
+    document.querySelector('.status-phone-number').innerHTML = "Input length must be 9-12";
+    return;
+  }
+  document.querySelector('.status-phone-number').innerHTML = "OK";
+  return;
 }
