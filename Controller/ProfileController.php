f29da2c8c38a77f3966fc1d670803f3461087d2e
daandeey <dandyarifrahman@gmail.com>
2018-10-25 10:34:37 +0700

    fix edit profile bug

--- Controller/ProfileController.php
+++ Controller/ProfileController.php
@@ -1,30 +1,28 @@
 <?php
-session_start();
-if (!class_exists('Config'))	{
-	include '../Config/Config.php';
-}
-include Config::DOCUMENT_ROOT . '/Model/Database.php';
+    if (!class_exists('Config'))	{
+        include '../Config/Config.php';
+    }
+    include Config::DOCUMENT_ROOT . '/Model/Database.php';
 
-class ProfileController	{
-	public static function fetchUser($username) {
-		$query = "SELECT * FROM user";
-		$users = Database::exec($query);
-		foreach ($users as $user) {
-			if ($username == $user["username"]) {
-				return $user;
-			}
-		}
-	}
-	public static function updateUser($username) {
-		$query = "UPDATE user SET name='" . $_POST['name'] . "', address='" . $_POST['address'] . "', phone-number='" . $_POST['phone-number'] . "' WHERE username='" . $username . "'";
-		Database::exec($query);
-		var_dump($query);
-	}
-}
+    class ProfileController	{
+        public static function fetchUser($username) {
+            $query = "SELECT * FROM user";
+            $users = Database::exec($query);
+            foreach ($users as $user) {
+                if ($username == $user["username"]) {
+                    return $user;
+                }
+            }
+        }
+        public static function updateUser($username) {
+            $query = "UPDATE user SET name='" . $_POST['name'] . "', address='" . $_POST['address'] . "', `phone-number`='" . $_POST['phone-number'] . "' WHERE username='" . $username . "'";
+            var_dump($query);
+            Database::exec($query);
+        }
+    }
 
-if (isset($_POST['save']))	{
-	ProfileController::updateUser($_SESSION['username']);
-	//header('Location: ' . Config::APP_URL . '/views/pages/EditProfile.php');
-}
-
+    if (isset($_POST['save']))	{
+        ProfileController::updateUser($_COOKIE['username']);
+        header('Location: ' . Config::APP_URL . '/views/pages/EditProfile.php');
+    }
 ?>
